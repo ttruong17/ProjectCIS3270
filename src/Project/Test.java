1d3335b70856049d49329e00fd384e9764c98a4a
vtenorio1 <vtenorio1@student.gsu.edu>
2014-11-21 23:21:28 -0500

    Test File
    tried to create a new customer

--- src/Project/Test.java
+++ src/Project/Test.java
@@ -1,8 +1,27 @@
 package project;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main (String [] args){
-		System.out.println("TST");
+		Scanner in = new Scanner(System.in);
+		System.out.println("first name");
+		String firstName = in.next();
+		System.out.println("last name");
+		String lastName= in.next();
+		System.out.println("middle name");
+		String middleName = in.next(); 
+		System.out.println("street num");
+		long streetNum = in.nextLong(); 
+		System.out.println("street name");
+		String streetName = in.next();
+		System.out.println(" city");
+		String city = in.next();
+		System.out.println("state");
+		String state = in.next();
+		System.out.println("zip code");
+		long zipCode= in.nextLong();
+		
 	}
 }
