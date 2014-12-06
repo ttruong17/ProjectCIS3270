9021b8472e081bd9c0356cb1c19a6ba685aae1cd
Anthony J.R. <anthonyjract@gmail.com>
2014-12-06 02:47:15 -0500

    Credentials Editing
    Trying to mess around with the Credentials return statements.

--- src/project/Credentials.java
+++ src/project/Credentials.java
@@ -6,7 +6,6 @@ public class Credentials {
 	String password;
 	
 	
-	
 	public Credentials() {
 
 	}
@@ -14,6 +13,39 @@ public class Credentials {
 	
 	public static void main(String[] args) {
 
+		
 	}
 
+
+	/**
+	 * @return the UserName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param UserName
+	 *            the firstName to set
+	 */
+	public void setUserName(String UserName) {
+		this.userName = UserName;
+	}
+
+	/**
+	 * @return the Password
+	 */
+	public String Password() {
+		return password;
+	}
+
+	/**
+	 * @param Password
+	 *            the Password to set
+	 */
+	public void setPassword(String Password) {
+		this.password = Password;
+	}
 }
+
+
