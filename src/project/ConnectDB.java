21c0da5dac5ee58b8dd419356fc002a05b546056
ttruong17 <07civic@gmail.com>
2014-12-12 11:46:41 -0500

    ConnectDB
    Snippet to connect to SQL Server

--- src/project/ConnectDB.java
+++ src/project/ConnectDB.java
@@ -1,6 +1,5 @@
 package project;
-
-import java.sql.Connection;
+import java.sql.*;
 
 public class ConnectDB {
 
@@ -11,11 +10,32 @@ public class ConnectDB {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	//this will connect to the database
+    public static void main(String a[]) throws ClassNotFoundException, SQLException
+    {
+        try
+        {
+            String url = "jdbc:sqlserver://localhost;databaseName=NightClub;integratedSecurity=true";   
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection conn = DriverManager.getConnection(url);
+            System.out.println("connection created");
+            Statement st=conn.createStatement();
+            String sql="select * from NightClub";
+            ResultSet rs=st.executeQuery(sql);
+            while(rs.next())
+            {
+                System.out.println("Name: "+rs.getString(1));
+                //System.out.println("Address : "+rs.getString(2));
+            }
+            if(st!=null)
+            st.close();
+            if(conn!=null)
+                conn.close();
+        }
+        catch(SQLException sqle)
+        {
+            System.out.println("Sql exception "+sqle);
+        }
+    }	//this will connect to the database
 	public Connection connect() {
 		return null;
 	}
